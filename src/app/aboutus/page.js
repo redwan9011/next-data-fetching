1471c041e605088ccaf68594c86d10848e565575
redwan9011 <redwan390309@gmail.com>
2024-04-30 00:02:22 +0600

    about page

---
+++ src/app/aboutus/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const aboutpage = () => {
+    return (
+        <div>
+            <h1>this is about us page</h1>
+        </div>
+    );
+};
+
+export default aboutpage;
